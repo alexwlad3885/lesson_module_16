d3b1b606decdce8f85aa0005e64003706f13d08f
alexwlad3885 <alexwlad3885@yandex.ru>
2024-09-30 22:16:36 +0300

    Домашнее задание по теме 'Валидация данных'

---
+++ module_16_2.py
@@ -0,0 +1,17 @@
+
+""" Домашнее задание по теме 'Валидация данных' """
+from fastapi import FastAPI, Path
+from typing import Annotated
+
+app = FastAPI()
+
+
+@app.get("/user/{user_id}")
+async def user_login(user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID", example="1")]) -> dict:
+    return {"message": f"Вы вошли как пользователь № {user_id}"}
+
+
+@app.get("/user/{username}/{age}")
+async def user_inf(username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")],
+                   age: Annotated[int, Path(ge=18, le=120, description="Enter age", example="24")]) -> dict:
+    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
